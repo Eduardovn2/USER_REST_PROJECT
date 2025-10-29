1577a22399fa404e0ee99b36cd8ddbfaa0dcdc0a
Eduardovn2 <Eduardovn985@gmail.com>
2025-10-29 20:29:54 -0300

    feat: Controller completo com todos os caminhos informados na criação,lista, atualização, deletação do UserService

---
+++ src/main/java/CadrastroUsuario/Project/Controller/UserController.java
@@ -0,0 +1,53 @@
+package CadrastroUsuario.Project.Controller;
+
+import CadrastroUsuario.Project.Entity.User;
+import CadrastroUsuario.Project.Service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/Register")
+public class UserController {
+
+    public UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping
+    User create(@RequestBody User user){
+        return userService.create(user);
+    }
+
+    @GetMapping
+    List<User> allList(){
+        return userService.list();
+    }
+
+    @PutMapping
+    List<User> replace(@RequestBody User user){
+        return userService.replace(user);
+    }
+
+    @DeleteMapping("{id}")
+    List<User> delete(@PathVariable("id") Long id){
+        return userService.delete(id);
+    }
+
+
+
+
+
+}
